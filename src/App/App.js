76aeb74be583b4dcf7d0aab50c1dac6d557358c5
Nhi Nguyen <nnguy152@gmail.com>
2018-04-17 20:40:58 -0400

    got keyboard to change colors on click

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Keyboard from '../Keyboard/Keyboard'
 
 // source from https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
 // allows me to require all files from a directory without having to import every single one
@@ -29,12 +28,14 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      selected: 'Wind Chimes'
+      selected: 'Wind Chimes',
+      active: false
     }
     this.handleKeyDown = this.handleKeyDown.bind(this)
     this.handleKeyUp = this.handleKeyUp.bind(this)
     this.handleSelected = this.handleSelected.bind(this)
     this.makeTones = this.makeTones.bind(this)
+    this.toggle = this.toggle.bind(this)
   }
 
   // makes the whole page have a listener
@@ -44,7 +45,6 @@ class App extends Component {
     this.makeTones()
   }
 
-
   // makes a range of frequencies
   makeTones () {
     let start = 1000
@@ -52,24 +52,28 @@ class App extends Component {
         start = start - 30
         tones.push(start)
     }
-    console.log(tones)
   }
 
   // plays a note base on keyboard selection
   handleKeyDown (e) {
     e.preventDefault()
+    this.toggle()
     if (this.state.selected === 'Triangle' || this.state.selected === 'Sawtooth' || this.state.selected === 'Sine') {
       console.log('keydown')
       this.createOscSounds()
       for (let i = 0; i < keyCode.length; i++) {
-          // changes pitch of each sound
-          if (e.keyCode === keyCode[i]) {
-            oscillator.frequency.value = tones[i]
-          }
+        // changes pitch of each sound
+        if (e.keyCode === keyCode[i]) {
+          oscillator.frequency.value = tones[i]
         }
+      }
     }
-     for (let i = 0; i < keyCode.length; i++) {
+
+    for (let i = 0; i < keyCode.length; i++) {
       if (e.keyCode === keyCode[i]) {
+        // this.setState({keyLetter: String.fromCharCode(keyCode[i])})
+          let key = document.getElementById(`${i}`) 
+          key.style.backgroundColor = 'rgb(143, 242, 255)'
         if (this.state.selected === 'Wind Chimes') {
           document.getElementById(`audio${[i]}`).play()
         } else if (this.state.selected === 'Piano') {
@@ -98,16 +102,18 @@ class App extends Component {
 
     oscillator.start(context.currentTime)
 
-      // stops oscillator after 0.2 seconds
+    // stops oscillator after 0.2 seconds
     oscillator.stop(context.currentTime + 0.2)
   }
 
-   // allows note to be played without waiting for sound file to end
-   handleKeyUp (e) {
-     for (let i = 0; i < keyCode.length; i++) {
+  // allows note to be played without waiting for sound file to end
+  handleKeyUp (e) {
+    for (let i = 0; i < keyCode.length; i++) {
       if (e.keyCode === keyCode[i]) {
+        let key = document.getElementById(`${i}`) 
+        key.style.backgroundColor = 'rgba(255,255,255, 0.55)'
         if (this.state.selected === 'Wind Chimes') {
-        document.getElementById(`audio${[i]}`).currentTime = 0;
+          document.getElementById(`audio${[i]}`).currentTime = 0;
         } else if (this.state.selected === 'Piano') {
           document.getElementById(`audio${[i + 25]}`).currentTime = 0;
         } 
@@ -115,19 +121,31 @@ class App extends Component {
     }
   }
 
-   handleSelected (e) {
-     let selected = e.target.value
-     this.setState({selected: selected})
-   }
+  handleSelected (e) {
+    let selected = e.target.value
+    this.setState({selected: selected})
+  }
+
+
+  toggle (e) {
+    console.log(this.state.active)
+    // this.setState((prevState) => { return { active: !prevState.active }} )
+  }
 
   render() {
-    // console.log(this.state.selected)
     // creates all the sound files to be played
     let music = Object.keys(soundFiles).map((file, i) => {
        return (
           <audio key={i} onKeyDown={this.handleKeyDown} onKeyUp={this.handleKeyUp} id={`audio${i}`} controls src={soundFiles[`${file}`]} />
         )
-      })
+    })
+
+    // creates keyboard on screen
+    let keyboard = keyCode.map((letter, i) => {
+      return ( 
+        <div key={i} className={`keys num${i}`} id={i}>{String.fromCharCode(letter)}</div>
+      )
+    })
 
     return (
       <div className="App">
@@ -139,8 +157,13 @@ class App extends Component {
           <option className="option">Sawtooth</option>
           <option className="option">Sine</option>
         </select>
+
+          <div className="random"></div>
           {music}
-          <Keyboard />
+
+          <div className="keyboard">
+            {keyboard}
+          </div>
       </div>
     );
   }
--- src/Keyboard/Keyboard.js
+++ src/Keyboard/Keyboard.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import './Keyboard.css'
+// import React, { Component } from 'react';
+// import './Keyboard.css'
 
-let keyboardLetters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h','j','k','l','z','x','c','v','b','n','m']
+// let keyboardLetters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h','j','k','l','z','x','c','v','b','n','m']
 
-class Keyboard extends Component {
+// class Keyboard extends Component {
 
-  render() {
-    let keyboard = keyboardLetters.map(letter => {
-      console.log(letter.toUpperCase())
-      return ( 
-        <div className="keys">{letter.toUpperCase()}</div>
-      )
-    })
-    return (
-      <div className="keyboard">
-        {keyboard}
-      </div>
-    );
-  }
-}
+//   render() {
+//     // console.log(this.props.keyLetter)
+//     console.log(this.props.active)
+//     let keyboard = keyboardLetters.map(letter => {
+//       return ( 
+//         <div className="keys" style={{background: this.props.active ? "rgba(255,255,255, 0.55)" : "none" }}>{letter.toUpperCase()}</div>
+//       )
+//     })
+//     return (
+//       <div className="keyboard">
+//         {keyboard}
+//       </div>
+//     );
+//   }
+// }
 
-export default Keyboard;
+// export default Keyboard;
